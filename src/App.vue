09d96f4b164fdcfc91b740bd6e7956a9ee4d0d15
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-08-24 15:10:08 +0200

    finish-header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <script>
-  export default{
+  import TheHeader from "../src/components/TheHeader.vue";
+  import theFooter from "../src/components/TheFooter.vue";
 
+  export default{
+    components:{
+      TheHeader,
+      theFooter,
+    }
   }
+  
 </script>
 
 <template>
+  
+  <TheHeader></TheHeader>
+  
+  <main>
+
+  </main>
+
+  <theFooter></theFooter>
+
 </template>
 
-<style scoped>
+<style>
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,28 @@
+<script>
+    export default{
+
+    }
+</script>
+
+<template>
+    <header>
+        <div class="top-container d-flex align-items-center gap-3">
+            <img src="../assets/header-logo.png" alt="" class="top-logo">
+            <h1 class="text-white fs-3">Yu-Gi-Oh API</h1>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+    .top-container{
+        background-image: url(../assets/bg-header.webp);
+        background-repeat: no-repeat;
+        background-size: cover;
+        padding: 1rem;
+
+        .top-logo{
+            width: 100px;
+        }
+    }
+
+</style>
