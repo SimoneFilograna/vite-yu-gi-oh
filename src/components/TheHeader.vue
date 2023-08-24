0312a2a0ac8d94230fec0f993f2860243dfd1eda
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-08-24 18:38:59 +0200

    style-header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -6,7 +6,7 @@
 
 <template>
     <header>
-        <div class="top-container d-flex align-items-center gap-3">
+        <div class="top-container d-flex align-items-center gap-3 px-5">
             <img src="../assets/header-logo.png" alt="" class="top-logo">
             <h1 class="text-white fs-3">Yu-Gi-Oh API</h1>
         </div>
@@ -18,10 +18,9 @@
         background-image: url(../assets/bg-header.webp);
         background-repeat: no-repeat;
         background-size: cover;
-        padding: 1rem;
 
         .top-logo{
-            width: 100px;
+            width: 150px;
         }
     }
 
